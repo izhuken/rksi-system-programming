d6d0e553d9bf2070a25e6e6b98ec3266be6675fc
Alexander <dctrback@bk.ru>
2024-09-29 18:09:49 +0300

    fix count odd))))

--- homework_6/task_4.cpp
+++ homework_6/task_4.cpp
@@ -41,7 +41,7 @@ float countOdd(int *array, int size)
 
     for (int i = 0; i < size; i++)
     {
-        if (array[i] % 2 != 0)
+        if (array[i] % 2 == 0)
         {
             result++;
         }
